89cd812179ce201cd7c5be1d14909a3dfd9c00bf
Zeph LaFassett <zelafassett@gmail.com>
2017-04-12 09:33:42 -0700

    fixing class error

--- story.php
+++ story.php
@@ -3,7 +3,7 @@
 	<?php require_once('snippets/header.php'); ?>
 </head>
 <body>
-	<div id="container">
+	<div class="container">
 		<?php require_once('snippets/menu.php'); ?>
 		<?php
 			// use sides 4
@@ -13,21 +13,21 @@
 
 		<div class="three-column">
 
-			<h2 class="story-header-2">The quiet kill off the country road, the time before the time when everything went wrong, and the last 16 hours of that great love affair - these are the seminal beginnings of oxbow.</h2>
-			<div class="portrait-wrapper">
-				<img src="/assets/img/portrait.jpg" alt="" />
-			</div>
-			<p>
-				Designed to be the last aural will and testament of failed humanity, OXBOW actually garnered listeners from among the ranks of the fucked, with their 1989 release FUCKFEST (cfy; (and later, pathological)).
-				Followed by KING OF THE JEWS (1991) (cfy), THE BALLS IN THE GREAT MEAT GRINDER COLLECTION (1991) (pathological), LET ME BE A WOMAN (1995) (brinkman/crippled dick hot wax) recorded by the estimable STEVE ALBINI, SERENADE IN RED (SST (1997)/CDHW (1996)) also recorded by ALBINI and GIBBS CHAPMAN (FAITH NO MORE, RED HOUSE PAINTERS), AN EVIL HEAT (2002) (neurot) by GIBBS CHAPMAN, LOVE THAT'S LAST (CD+DVD) (2006) (Hydra Head) by various, and THE NARCOTIC STORY (2007) (Hydra Head) co-produced by JOE CHICCARELLI (THE RACONTEURS, FRANK ZAPPA, ELTON JOHN), and NIKO WENNER, as well as various singles and dance remixes, OXBOW has met with increasing critical acclaim and popular support.
-			</p>
-			<p>
-				The OXBOW core membership -- DAN ADAMS/bass, GREG DAVIS/drums, EUGENE ROBINSON/voice, and NIKO WENNER/guitars+keyboards -- has been supplemented by the subtle stylings of LYDIA LUNCH, KLAUS FLOURIDE, JARBOE, MARIANNE FAITHFULL, RICHARD KERN (SONIC YOUTH et al), JON RASKIN from the ROVA SAX QUARTET, and a string section that would make Barry White wince.
-			</p>
-			<p>
-				Though claiming San Francisco as their hometown, OXBOW played in England in 1990 and toured Europe and the U.S. in 1995, 1996, 1998, 1999, 2002, 2003, and 2007. OXBOW has also performed in Tokyo, Osaka, London, Berlin, New York, Vienna, Oslo, Prague, Portland, Paris, Copenhagen, Amsterdam, Rome, Los Angeles, Seattle, San Francisco, Gent, and almost every other place people will pay them to hear them play.
-			</p>
-
+				<h2 class="story-header-2">The quiet kill off the country road, the time before the time when everything went wrong, and the last 16 hours of that great love affair - these are the seminal beginnings of oxbow.</h2>
+				<div class="portrait-wrapper">
+					<img src="/assets/img/portrait.jpg" alt="" />
+				</div>
+				<p>
+					Designed to be the last aural will and testament of failed humanity, OXBOW actually garnered listeners from among the ranks of the fucked, with their 1989 release FUCKFEST (cfy; (and later, pathological)).
+					Followed by KING OF THE JEWS (1991) (cfy), THE BALLS IN THE GREAT MEAT GRINDER COLLECTION (1991) (pathological), LET ME BE A WOMAN (1995) (brinkman/crippled dick hot wax) recorded by the estimable STEVE ALBINI, SERENADE IN RED (SST (1997)/CDHW (1996)) also recorded by ALBINI and GIBBS CHAPMAN (FAITH NO MORE, RED HOUSE PAINTERS), AN EVIL HEAT (2002) (neurot) by GIBBS CHAPMAN, LOVE THAT'S LAST (CD+DVD) (2006) (Hydra Head) by various, and THE NARCOTIC STORY (2007) (Hydra Head) co-produced by JOE CHICCARELLI (THE RACONTEURS, FRANK ZAPPA, ELTON JOHN), and NIKO WENNER, as well as various singles and dance remixes, OXBOW has met with increasing critical acclaim and popular support.
+				</p>
+				<p>
+					The OXBOW core membership -- DAN ADAMS/bass, GREG DAVIS/drums, EUGENE ROBINSON/voice, and NIKO WENNER/guitars+keyboards -- has been supplemented by the subtle stylings of LYDIA LUNCH, KLAUS FLOURIDE, JARBOE, MARIANNE FAITHFULL, RICHARD KERN (SONIC YOUTH et al), JON RASKIN from the ROVA SAX QUARTET, and a string section that would make Barry White wince.
+				</p>
+				<p>
+					Though claiming San Francisco as their hometown, OXBOW played in England in 1990 and toured Europe and the U.S. in 1995, 1996, 1998, 1999, 2002, 2003, and 2007. OXBOW has also performed in Tokyo, Osaka, London, Berlin, New York, Vienna, Oslo, Prague, Portland, Paris, Copenhagen, Amsterdam, Rome, Los Angeles, Seattle, San Francisco, Gent, and almost every other place people will pay them to hear them play.
+				</p>
+			
 		</div> <!-- /three-column -->
 	</div> <!-- /container -->
 	<?php require_once('snippets/footer.php'); ?>
